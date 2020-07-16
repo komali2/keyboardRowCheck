d0b173ab5128b90ce039e7182b3a3b74101a13a8
Caleb Rogers <rogersjcaleb@gmail.com>
2020-07-15 19:35:57 -0700

    Basic styling!

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
-    <input v-model="userInput" />
-    <div v-if="isValid(userInput)">
-      It's valid!
-    </div>
-    <div v-if="!isValid(userInput)">
-      It's not valid :(
-    </div>
-    <select v-model="userSelectedLayout">
-      <option disabled value="">Please select keyboard layout</option>
-      <option value="usstandard">US Standard QWERTY</option>
-      <option value="dvorakus">Dvorak Simplified (US)</option>
-    </select>
-  </div>
+  <main>
+    <header>
+      <h1>
+        Is your text on a single row?
+        <small>
+          Insert it below to find out!
+        </small>
+      </h1>
+    </header>
+    <section>
+      <input v-model="userInput" />
+      <div v-if="isValid(userInput)">
+        It's valid!
+      </div>
+      <div v-if="!isValid(userInput)">
+        It's not valid :(
+      </div>
+      <select v-model="userSelectedLayout">
+        <option disabled value="">Please select keyboard layout</option>
+        <option value="usstandard">US Standard QWERTY</option>
+        <option value="dvorakus">Dvorak Simplified (US)</option>
+      </select>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -123,12 +133,64 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #f4f4f4;
+  font-family: helvetica neue,helvetica,sans-serif;
+  line-height: 1.4;
 }
+main {
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 34em;
+}
+header {
+  font-size: 2em;
+  line-height: 1.1;
+}
+
+h1 small, h2 small, h3 small, h4 small {
+  display: block;
+}
+
+h3 small {
+  font-weight: normal;
+}
+
+main section {
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+}
+main section div {
+   margin: 8px 0px;
+ }
+main section input {
+   margin: 8px 0px;
+ }
+ main section select {
+   margin: 8px 0px;
+ }
+
+main small {
+  font-weight: normal
+}
+
+@media only screen {
+
+ } /* Define mobile styles */
+
+@media only screen and (min-width: 40.063em) {
+ } /* min-width 641px, medium screens */
+
+@media only screen and (min-width: 64.063em) {
+ } /* min-width 1025px, large screens */
+
+@media only screen and (min-width: 90.063em) { } /* min-width 1441px, xlarge screens */
+
+@media only screen and (min-width: 120.063em) { } /* min-width 1921px, xxlarge screens */
+
 </style>
