bc052b173c7cc58f8fdf5bfa8f33502f13e368ba
Caleb Rogers <rogersjcaleb@gmail.com>
2020-07-15 19:24:11 -0700

    initial program working

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,125 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input v-model="userInput" />
+    <div v-if="isValid(userInput)">
+      It's valid!
+    </div>
+    <div v-if="!isValid(userInput)">
+      It's not valid :(
+    </div>
+    <select v-model="userSelectedLayout">
+      <option disabled value="">Please select keyboard layout</option>
+      <option value="usstandard">US Standard QWERTY</option>
+      <option value="dvorakus">Dvorak Simplified (US)</option>
+    </select>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      userInput: '',
+      userSelectedLayout: '',
+      layouts: {
+        usstandard: [
+          [
+            'A',
+            'S',
+            'D',
+            'F',
+            'G',
+            'H',
+            'J',
+            'K',
+            'L'],
+          [
+            'Q',
+            'W',
+            'E',
+            'R',
+            'T',
+            'Y',
+            'U',
+            'I',
+            'O',
+            'P',
+          ], [
+            'Z',
+            'X',
+            'C',
+            'V',
+            'N',
+            'B',
+            'N',
+            'M',
+          ],
+        ],
+        dvorakus: [
+          [
+            'P',
+            'Y',
+            'F',
+            'G',
+            'C',
+            'R',
+            'L',
+          ], [
+            'A',
+            'O',
+            'E',
+            'U',
+            'I',
+            'D',
+            'H',
+            'T',
+            'N',
+            'S',
+          ], [
+            'Q',
+            'J',
+            'K',
+            'X',
+            'B',
+            'M',
+            'W',
+            'V',
+            'Z',
+          ],
+        ],
+      },
+    };
+  },
+  methods: {
+    isValid(input) {
+      if (!input.length || !this.userSelectedLayout.length) {
+        return false;
+      }
+      // Determine which row this string may be on
+      let keyRow = null;
+      const layout = this.layouts[this.userSelectedLayout];
+
+      // eslint-disable-next-line
+      layout.forEach((row)=>{
+        if (row.includes(input[0].toUpperCase())) {
+          keyRow = row;
+        }
+      });
+      if (!keyRow) return false;
+
+      // eslint-disable-next-line
+      for (let j = 0; j < input.length; j++) {
+        if (!keyRow.includes(input[j].toUpperCase())) {
+          return false;
+        }
+      }
+      return true;
+    },
   },
 };
+
 </script>
 
 <style>
